0bbe08413c893fa621efbadf8953d61bdbba7cb1
lafayettegabe <gabriel-soares@uergs.edu.br>
2024-08-04 01:22:03 -0300

    fix(fundamentals): fmi prettier

--- fundamentals/structs_methods_n_interfaces/shapes_test.go
+++ fundamentals/structs_methods_n_interfaces/shapes_test.go
@@ -3,14 +3,14 @@ package main
 import "testing"
 
 func TestPerimeter(t *testing.T) {
-  want := 40.0
+	want := 40.0
 
-  rectangle := Rectangle{10.0, 10.0}
-  got := Perimeter(rectangle)
+	rectangle := Rectangle{10.0, 10.0}
+	got := Perimeter(rectangle)
 
-  if want != got {
-    t.Errorf("got %.2f want %.2f", got, want)
-  }
+	if want != got {
+		t.Errorf("got %.2f want %.2f", got, want)
+	}
 }
 
 func TestArea(t *testing.T) {
@@ -36,24 +36,24 @@ func TestArea(t *testing.T) {
 func TestAreaTable(t *testing.T) {
 
 	areaTests := []struct {
-    name string
-		shape Shape
-		hasArea  float64
+		name    string
+		shape   Shape
+		hasArea float64
 	}{
-    {
-      name: "Rectangle",
-      shape: Rectangle{Width: 12, Height: 6}, 
-      hasArea: 72.0,
-    },{
-      name: "Circle",
-      shape: Circle{Radius: 10}, 
-      hasArea: 314.1592653589793,
-    },{
-      name: "Triangle",
-      shape: Triangle{Base: 12, Height: 6}, 
-      hasArea: 36.0,
-    },
-  }
+		{
+			name:    "Rectangle",
+			shape:   Rectangle{Width: 12, Height: 6},
+			hasArea: 72.0,
+		}, {
+			name:    "Circle",
+			shape:   Circle{Radius: 10},
+			hasArea: 314.1592653589793,
+		}, {
+			name:    "Triangle",
+			shape:   Triangle{Base: 12, Height: 6},
+			hasArea: 36.0,
+		},
+	}
 
 	for _, tt := range areaTests {
 		t.Run(tt.name, func(t *testing.T) {
